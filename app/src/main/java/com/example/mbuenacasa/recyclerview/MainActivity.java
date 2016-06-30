bf69b3bde720b95172f173e0432106c6c98f00ce
mbuenacasa <mbuenacasa@opentrends.net>
2016-06-30 10:00:00 +0200

    [WIP] Fragment funcional, a por el RecyclerView :___D

--- app/src/main/java/com/example/mbuenacasa/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/mbuenacasa/recyclerview/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.mbuenacasa.recyclerview;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        android.app.FragmentManager fragmentManager = getFragmentManager();
+        android.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        MyCustomFragment myCustomFragment = new MyCustomFragment();
+        fragmentTransaction.add(R.id.fragmentlayout,myCustomFragment);
+        fragmentTransaction.commit();
     }
 }
---
+++ app/src/main/java/com/example/mbuenacasa/recyclerview/MyCustomFragment.java
@@ -0,0 +1,51 @@
+package com.example.mbuenacasa.recyclerview;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by mbuenacasa on 30/06/16.
+ */
+public class MyCustomFragment extends Fragment implements View.OnClickListener{
+
+    private Button holaButton;
+    private TextView textView;
+    private int clickCounter;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater,container,savedInstanceState);
+        View view = inflater.inflate(R.layout.my_custom_fragment,container,false);
+
+        clickCounter =0;
+        holaButton = (Button) view.findViewById(R.id.holaButton);
+        textView = (TextView) view.findViewById(R.id.textView);
+
+        setViews();
+        return view;
+    }
+
+    private void setViews(){
+        holaButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == holaButton){
+            clickCounter++;
+            textView.setText("Hola"+Integer.toString(clickCounter));
+        }
+    }
+}
