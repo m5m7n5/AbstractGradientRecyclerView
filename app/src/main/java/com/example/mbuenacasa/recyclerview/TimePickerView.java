c75a461a3168e233fbb1b05f17cf75229962c307
mbuenacasa <mbuenacasa@opentrends.net>
2016-07-15 13:19:43 +0200

    [DOC] Explanation of the TimePickerView added

--- app/src/main/java/com/example/mbuenacasa/recyclerview/TimePickerView.java
+++ app/src/main/java/com/example/mbuenacasa/recyclerview/TimePickerView.java
@@ -7,6 +7,7 @@ import android.widget.RelativeLayout;
 
 /**
  * Created by mbuenacasa on 14/07/16.
+ * Custom class of the timePickerView that places SimpleStringRecycler
  */
 public class TimePickerView extends RelativeLayout{
 
@@ -14,31 +15,49 @@ public class TimePickerView extends RelativeLayout{
     private SimpleStringRecyclerView hours;
     private SimpleStringRecyclerView minutes;
 
+    /**
+     * Custom constructor
+     * @param context
+     */
     public TimePickerView(Context context) {
         super(context);
         inflater = LayoutInflater.from(context);
         init();
     }
 
+    /**
+     * Custom constructor
+     * @param context
+     * @param attrs
+     */
     public TimePickerView(Context context, AttributeSet attrs) {
         super(context, attrs);
         inflater = LayoutInflater.from(context);
         init();
     }
 
+    /**
+     * Custom constructor
+     * @param context
+     * @param attrs
+     * @param defStyleAttr
+     */
     public TimePickerView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         inflater = LayoutInflater.from(context);
         init();
     }
 
+    /**
+     * Method that initializes the view and his components
+     */
     private void init(){
         inflater.inflate(R.layout.view_new_hours_selector,this,true);
         hours = (SimpleStringRecyclerView) findViewById(R.id.new_hours_view_hours_recycler);
-        hours.initAdapter(hours.generateHoursNumbers());
+        hours.setAdapterList(hours.generateHoursNumbers());
 
         minutes = (SimpleStringRecyclerView) findViewById(R.id.new_hours_view_minutes_recycler);
-        minutes.initAdapter(minutes.generateMinutesNumbers());
+        minutes.setAdapterList(minutes.generateMinutesNumbers());
 
     }
 
@@ -50,18 +69,34 @@ public class TimePickerView extends RelativeLayout{
         return hours.getSelectedString() +":"+ minutes.getSelectedString();
     }
 
+    /**
+     * Method that returns the selected hour as an int
+     * @return
+     */
     public int getSelectedHour(){
         return Integer.parseInt(hours.getSelectedString());
     }
 
+    /**
+     * Method that returns the selected minute as an int
+     * @return
+     */
     public int getSelectedMinute(){
         return Integer.parseInt(minutes.getSelectedString());
     }
 
+    /**
+     * Method that returns the timestamp value of the selected hour, in seconds
+     * @return
+     */
     public int getTimestampSeconds(){
         return getSelectedHour()*3600+getSelectedMinute()*60;
     }
 
+    /**
+     * Method that returns the timestamp value of the selected hour, in miliseconds
+     * @return
+     */
     public int getTimeStampMiliSeconds(){
         return getTimestampSeconds()*1000;
     }
